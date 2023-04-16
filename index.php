09889222fa29b2cd508909493c4cb6a6e27d449c
94AndresRodriguez <richardjokerazul@gmail,com>
2023-04-16 01:04:32 -0500

    cambios archivo html

--- index.php
+++ index.php
@@ -3,4 +3,7 @@
     $secondNumber = 4;
 
     echo $firtsNumber + $secondNumber;
-    ?>
+    echo $firstNumber - $second Number;
+    echo $firtsNumber * $secondNumber;
+    echo $firtsNumber / $secondNumber;
+   ?> 
